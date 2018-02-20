5ef2eafa4c75f751bcd11f420af4999bc308b778
Eike Spielberg <Eike.Spielberg@uni-due.de>
2018-02-20 08:45:11 +0100

    real getter instead of dummy data

--- src/app/service/getter.service.ts
+++ src/app/service/getter.service.ts
@@ -16,8 +16,8 @@ export class GetterService {
 
   getFullManifestation(shelfmark: string, exact: boolean): Observable<Manifestation[]> {
     const manifestationsFound = this.http.get<Manifestation[]>(
-       'assets/data/example.json'
-      // appGlobals.getterUrl + '/fullManifestation?identifier=' + shelfmark.trim() + '&exact=' + exact
+      // 'assets/data/example.json'
+      appGlobals.getterUrl + '/fullManifestation?identifier=' + shelfmark.trim() + '&exact=' + exact
     , { headers: new HttpHeaders({ timeout: `${60000}` }) });
     manifestationsFound.subscribe(data => this.manifestations = data);
     return manifestationsFound;
